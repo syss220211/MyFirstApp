9615331d3b55975f19f89ee0000df99a64713169
syss220211 <syss220211@gmail.com>
2022-10-15 11:30:15 +0900

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by geonhui Yu on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
